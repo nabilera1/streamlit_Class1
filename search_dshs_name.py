85d62f4a90814f955f2d6bcbf2213a3e9fe9ab32
김동균 <nabilera1@gmail.com>
2025-04-10 08:03:32 +0900

    Update search_dshs_name.py

--- search_dshs_name.py
+++ search_dshs_name.py
@@ -1,8 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-st.title("🔐 사용자 ID / 비밀번호 조회")
-st.info("비밀번호는 처음 접속 후 변경을 권장합니다.")
+st.title("사용자 ID / 비밀번호 조회")
+st.info("금요일 이후 이 사이트는 사라짐. \n 아이디 비번 메모 및 비밀번호는 처음 접속 후 변경을 권장합니다.")
+
+
+st.markdown("""
+---
+
+### 🔍 [검수 완료를 위한 테스트 방법]  
+- **ChatGPT 접속 사이트**  
+👉 [https://chatgpt.com/](https://chatgpt.com/)
+
+---
+
+### 🔐 [인증코드 요구시 참고 사이트]  
+- 처음 접속 시 인증코드를 요청하면 **mygpt.kr** 사이트에 아래 아이디로 로그인하면 됩니다.  
+  - **아이디**: `dshs04`  
+  - **비밀번호**: 위와 동일  
+  👉 [https://www.mygpt.kr/](https://www.mygpt.kr/)
+
+---
+""")
+
 
 # 엑셀 파일 경로 or 구글 드라이브 URL
 url = "https://docs.google.com/spreadsheets/d/1XIWlTtMlsL07eoyJf8sbf7SjJWXBZxd2/edit?usp=sharing&ouid=102499631843628462810&rtpof=true&sd=true"
